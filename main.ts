947b3490773b03375378a0a981f9cbbc7b47849f
Ramin Azhdari <47226721+raminos@users.noreply.github.com>
2021-02-24 08:30:09 +0100

    add ability to transform camel and pascal cases

--- main.ts
+++ main.ts
@@ -1,9 +1,44 @@
 import { parse } from "https://deno.land/std/flags/mod.ts";
 import { readLines } from "https://deno.land/std/io/bufio.ts";
 
-const extractLowerCaseWords = (input: string): Array<string> => {
-  return input.toLowerCase().split(/[ _-]/g);
+const pipe = <R>(...fns: Array<(a: R) => R>) => (
+  fns.reduce((prevFn, nextFn) => (value) => nextFn(prevFn(value)))
+);
+
+const splitWords = (input: string): Array<string> => {
+  return input.split(/[ _-]/g);
 };
+
+const joinArraysOfLettersToWords = (arraysOfLetters: Array<Array<string>>): Array<string> => (
+  arraysOfLetters.reduce((wordArray, letterArray) => {
+    wordArray.push(letterArray.join(""));
+    return wordArray;
+  }, [])
+)
+
+const splitWordsWithCaseChange = (words: Array<string>): Array<string> => {
+  return words.reduce((newWords: Array<string>, word) => {
+    let isWordBeginning = true;
+    let splittedWordsLetters: Array<Array<string>> = [[]];
+
+    for (const letter of word) {
+      if (letter.match(/[A-Z]/) && !isWordBeginning) {
+        splittedWordsLetters.push([letter]);
+        isWordBeginning = true;
+      } else {
+        splittedWordsLetters[splittedWordsLetters.length - 1].push(letter);
+        isWordBeginning = false;
+      }
+    }
+
+    const splittedWords = joinArraysOfLettersToWords(splittedWordsLetters)
+    return newWords.concat(splittedWords);
+  }, []);
+};
+
+const lowerCaseWords = (words: Array<string>): Array<string> => (
+  words.map((word) => word.toLowerCase())
+);
 
 const removeNonAlphaNumericChars = (
   unfilteredWords: Array<string>,
@@ -17,9 +52,11 @@ const createKebabCase = (words: Array<string>): string => words.join("-");
 
 const createSnakeCase = (words: Array<string>): string => words.join("_");
 
-const createFlatCase = (words: Array<string>): string => words.join("")
+const createFlatCase = (words: Array<string>): string => words.join("");
 
-const createPascalCase = (words: Array<string>): string => upperCaseWords(words).join("")
+const createPascalCase = (words: Array<string>): string => (
+  upperCaseWords(words).join("")
+);
 
 const createCamelCase = (words: Array<string>): string => {
   const [first, ...rest] = words;
@@ -71,8 +108,12 @@ async function main(args: Array<string>): Promise<void> {
   }
 
   const input = argumentInput ? String(argumentInput) : pipedInput;
-  const cleanedInput = extractLowerCaseWords(input);
-  const filteredInput = removeNonAlphaNumericChars(cleanedInput);
+  const splittedWords = splitWords(input);
+  const filteredInput = pipe(
+    removeNonAlphaNumericChars,
+    splitWordsWithCaseChange,
+    lowerCaseWords,
+  )(splittedWords);
 
   if (kebab) console.log(createKebabCase(filteredInput));
   if (snake) console.log(createSnakeCase(filteredInput));
